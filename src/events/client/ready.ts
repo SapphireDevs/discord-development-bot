3d26d322139b00a2fc7d065aff2d4e145ae821e3
AmitKumarHQ <amitkumar100fficial@gmail.com>
2023-05-24 03:19:48 +0530

    style: added prettier and eslint

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -1,41 +1,42 @@
-import { DiscordClient } from "../../bot.js";
-import { EventInterface } from "../../typings/index.js";
-import { Events, ActivityType } from "discord.js";
-import { getGuilds, getUsers } from "../../helpers/discord.js";
+import { DiscordClient } from '../../bot.js';
+import { EventInterface } from '../../typings/index.js';
+import { Events, ActivityType } from 'discord.js';
+import { getGuilds, getUsers } from '../../helpers/discord.js';
 
 const event: EventInterface = {
-  name: Events.ClientReady,
-  options: { once: true, rest: false },
-  execute: async (client: DiscordClient) => {
-    async function getPresence() {
-      const presences = [
-        {
-          name: `${await getGuilds(client)} Guilds`,
-          type: ActivityType.Watching as const,
-        },
-        {
-          name: `${await getUsers(client)} Users`,
-          type: ActivityType.Watching as const,
-        },
-      ];
+    name: Events.ClientReady,
+    options: { once: true, rest: false },
 
-      const presence = presences[Math.floor(Math.random() * presences.length)];
+    execute: async (client: DiscordClient) => {
+        async function getPresence() {
+            const presences = [
+                {
+                    name: `${await getGuilds(client)} Guilds`,
+                    type: ActivityType.Watching as const,
+                },
+                {
+                    name: `${await getUsers(client)} Users`,
+                    type: ActivityType.Watching as const,
+                },
+            ];
 
-      client.user?.setPresence({
-        status: "online",
-        activities: [
-          {
-            name: presence.name,
-            type: presence.type,
-          },
-        ],
-      });
-    }
+            const presence = presences[Math.floor(Math.random() * presences.length)];
 
-    await getPresence();
-    setInterval(async function () {
-      await getPresence();
-    }, 1800000);
-  },
+            client.user?.setPresence({
+                status: 'online',
+                activities: [
+                    {
+                        name: presence.name,
+                        type: presence.type,
+                    },
+                ],
+            });
+        }
+
+        await getPresence();
+        setInterval(async function () {
+            await getPresence();
+        }, 1800000);
+    },
 };
 export default event;
